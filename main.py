f1a9405125379bb38cc347555526312c4055473e
jerzmerner <rickjburner@gmail.com>
2022-02-27 14:25:34 -0500

    Working 9x9 motor map

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def ControlRightMotor(x: number, y: number):
-    return int(rMotorMap[Math.idiv(y - 500, 175) + 2][Math.idiv(x - 500, 175) + 2] * 255)
+    return int(rMotorMap[Math.idiv(y - 500, 103) + 4][Math.idiv(x - 500, 103) + 4] * 255)
 def ControlLeftMotor(x2: number, y2: number):
-    return int(lMotorMap[Math.idiv(y2 - 500, 175) + 2][Math.idiv(x2 - 500, 175) + 2] * 255)
+    serial.write_value("x", 0)
+    serial.write_value("x", 0)
+    serial.write_value("x", 0)
+    return int(lMotorMap[Math.idiv(y2 - 500, 103) + 4][Math.idiv(x2 - 500, 103) + 4] * 255)
 rMotorMap: List[List[number]] = []
 lMotorMap: List[List[number]] = []
 radio.set_group(1)
@@ -9,16 +12,24 @@ pins.set_pull(DigitalPin.P13, PinPullMode.PULL_NONE)
 pins.set_pull(DigitalPin.P14, PinPullMode.PULL_NONE)
 pins.set_pull(DigitalPin.P15, PinPullMode.PULL_NONE)
 pins.set_pull(DigitalPin.P16, PinPullMode.PULL_NONE)
-lMotorMap = [[-1, -1, -1, -0.5, 0],
-    [-1, -0.75, -0.5, 0, 0.5],
-    [-1, -0.5, 0, 0.5, 1],
-    [-0.5, 0, 0.5, 0.75, 1],
-    [0, 0.5, 1, 1, 1]]
-rMotorMap = [[0, -0.5, -1, -1, -1],
-    [0.5, 0, -0.5, -.75, -1],
-    [1, 0.5, 0, -0.5, -1],
-    [1, 0.75, 0.5, 0, -0.5],
-    [1, 1, 1, 0.5, 0]]
+lMotorMap = [[-1, -1, -1, -1, -1, -0.75, -0.5, -0.25, 0],
+    [-1, -0.9375, -0.875, -0.8125, -0.75, -0.5, -0.25, 0, 0.25],
+    [-1, -0.875, -0.75, -0.625, -0.5, -0.25, 0, 0.25, 0.5],
+    [-1, -0.8125, -0.625, -0.375, -0.25, 0, 0.25, 0.5, 0.75],
+    [-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1],
+    [-0.75, -0.5, -0.25, 0, 0.25, 0.375, 0.625, 0.8125, 1],
+    [-0.5, -0.25, 0, 0.25, 0.5, 0.625, 0.75, 0.875, 1],
+    [-0.25, 0, 0.25, 0.5, 0.75, 0.8125, 0.875, 0.9375, 1],
+    [0, 0.25, 0.5, 0.75, 1, 1, 1, 1, 1]]
+rMotorMap = [[0, -0.25, -0.5, -0.75, -1, -1, -1, -1, -1],
+    [0.25, 0, -0.25, -0.5, -0.75, -0.8125, -0.875, -0.9375, -1],
+    [0.5, 0.25, 0, -0.25, -0.5, -0.625, -0.75, -0.875, -1],
+    [0.75, 0.5, 0.25, 0, -0.25, -0.375, -0.625, -0.8125, -1],
+    [1, 0.75, 0.5, 0.25, 0, -0.25, -0.5, -0.75, -1],
+    [1, 0.8125, 0.625, 0.375, 0.25, 0, -0.25, -0.5, -0.75],
+    [1, 0.875, 0.75, 0.625, 0.5, 0.25, 0, -0.25, -0.5],
+    [1, 0.9375, 0.875, 0.8125, 0.75, 0.5, 0.25, 0, -0.25],
+    [1, 1, 1, 1, 1, 0.75, 0.5, 0.25, 0]]
 
 def on_forever():
     if pins.digital_read_pin(DigitalPin.P15) == 0:
--- main.ts
+++ main.ts
@@ -1,21 +1,222 @@
-function ControlRightMotor(x: number, y: number): number {
-    return Math.trunc(rMotorMap[Math.idiv(y - 500, 175) + 2][Math.idiv(x - 500, 175) + 2] * 255)
+function ControlRightMotor (x: number, y: number) {
+    return Math.trunc(rMotorMap[Math.idiv(y - 500, 103) + 4][Math.idiv(x - 500, 103) + 4] * 255)
 }
-
-function ControlLeftMotor(x2: number, y2: number): number {
-    return Math.trunc(lMotorMap[Math.idiv(y2 - 500, 175) + 2][Math.idiv(x2 - 500, 175) + 2] * 255)
+function ControlLeftMotor (x2: number, y2: number) {
+    serial.writeValue("x2", Math.idiv(x2 - 500, 105) + 4)
+    serial.writeValue("y2", Math.idiv(y2 - 500, 105) + 4)
+    serial.writeValue("mapVal", Math.trunc(lMotorMap[Math.idiv(y2 - 500, 105) + 4][Math.idiv(x2 - 500, 105) + 4] * 255))
+    return Math.trunc(lMotorMap[Math.idiv(y2 - 500, 105) + 4][Math.idiv(x2 - 500, 105) + 4] * 255)
 }
-
-let rMotorMap : number[][] = []
-let lMotorMap : number[][] = []
+let rMotorMap: number[][] = []
+let lMotorMap: number[][] = []
 radio.setGroup(1)
 pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
 pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
-lMotorMap = [[-1, -1, -1, -0.5, 0], [-1, -0.75, -0.5, 0, 0.5], [-1, -0.5, 0, 0.5, 1], [-0.5, 0, 0.5, 0.75, 1], [0, 0.5, 1, 1, 1]]
-rMotorMap = [[0, -0.5, -1, -1, -1], [0.5, 0, -0.5, -.75, -1], [1, 0.5, 0, -0.5, -1], [1, 0.75, 0.5, 0, -0.5], [1, 1, 1, 0.5, 0]]
-basic.forever(function on_forever() {
+lMotorMap = [
+[
+-1,
+-1,
+-1,
+-1,
+-1,
+-0.75,
+-0.5,
+-0.25,
+0
+],
+[
+-1,
+-0.9375,
+-0.875,
+-0.8125,
+-0.75,
+-0.5,
+-0.25,
+0,
+0.25
+],
+[
+-1,
+-0.875,
+-0.75,
+-0.625,
+-0.5,
+-0.25,
+0,
+0.25,
+0.5
+],
+[
+-1,
+-0.8125,
+-0.625,
+-0.375,
+-0.25,
+0,
+0.25,
+0.5,
+0.75
+],
+[
+-1,
+-0.75,
+-0.5,
+-0.25,
+0,
+0.25,
+0.5,
+0.75,
+1
+],
+[
+-0.75,
+-0.5,
+-0.25,
+0,
+0.25,
+0.375,
+0.625,
+0.8125,
+1
+],
+[
+-0.5,
+-0.25,
+0,
+0.25,
+0.5,
+0.625,
+0.75,
+0.875,
+1
+],
+[
+-0.25,
+0,
+0.25,
+0.5,
+0.75,
+0.8125,
+0.875,
+0.9375,
+1
+],
+[
+0,
+0.25,
+0.5,
+0.75,
+1,
+1,
+1,
+1,
+1
+]
+]
+rMotorMap = [
+[
+0,
+-0.25,
+-0.5,
+-0.75,
+-1,
+-1,
+-1,
+-1,
+-1
+],
+[
+0.25,
+0,
+-0.25,
+-0.5,
+-0.75,
+-0.8125,
+-0.875,
+-0.9375,
+-1
+],
+[
+0.5,
+0.25,
+0,
+-0.25,
+-0.5,
+-0.625,
+-0.75,
+-0.875,
+-1
+],
+[
+0.75,
+0.5,
+0.25,
+0,
+-0.25,
+-0.375,
+-0.625,
+-0.8125,
+-1
+],
+[
+1,
+0.75,
+0.5,
+0.25,
+0,
+-0.25,
+-0.5,
+-0.75,
+-1
+],
+[
+1,
+0.8125,
+0.625,
+0.375,
+0.25,
+0,
+-0.25,
+-0.5,
+-0.75
+],
+[
+1,
+0.875,
+0.75,
+0.625,
+0.5,
+0.25,
+0,
+-0.25,
+-0.5
+],
+[
+1,
+0.9375,
+0.875,
+0.8125,
+0.75,
+0.5,
+0.25,
+0,
+-0.25
+],
+[
+1,
+1,
+1,
+1,
+1,
+0.75,
+0.5,
+0.25,
+0
+]
+]
+basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P15) == 0) {
         radio.sendString("Open")
     } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
@@ -28,5 +229,4 @@ basic.forever(function on_forever() {
         radio.sendValue("lMotor", ControlLeftMotor(pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P2)))
         radio.sendValue("rMotor", ControlRightMotor(pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P2)))
     }
-    
 })
