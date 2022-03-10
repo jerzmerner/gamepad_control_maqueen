0b206c23c9ed5a0303d849040b157220e11731a3
jerzmerner <rickjburner@gmail.com>
2022-03-10 09:51:25 -0500

    Adjustable Motor Map and Bug Fixes

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-def ControlRightMotor(x: number, y: number):
-    return int(rMotorMap[Math.idiv(y - 500, 114) + 4.5][Math.idiv(x - 500, 114) + 4.5] * 255)
-def ControlLeftMotor(x2: number, y2: number):
-    return int(lMotorMap[Math.idiv(y2 - 500, 114) + 4.5][Math.idiv(x2 - 500, 114) + 4.5] * 255)
+def on_button_pressed_a():
+    radio.send_string("neoPixelR")
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    radio.send_string("neoPixelB")
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    radio.send_string("neoPixelG")
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def ControlMotor(x: number, y: number, motor: str):
+    if motor == "R":
+        return int(rMotorMap[Math.idiv(y - 500, 114) + 4.5][Math.idiv(x - 500, 114) + 4.5] * maxMotorSpeed)
+    else:
+        return int(lMotorMap[Math.idiv(y - 500, 114) + 4.5][Math.idiv(x - 500, 114) + 4.5] * maxMotorSpeed)
+rMotorSpeed = 0
+lMotorSpeed = 0
 rMotorMap: List[List[number]] = []
 lMotorMap: List[List[number]] = []
 radio.set_group(1)
-pins.set_pull(DigitalPin.P13, PinPullMode.PULL_NONE)
-pins.set_pull(DigitalPin.P14, PinPullMode.PULL_NONE)
-pins.set_pull(DigitalPin.P15, PinPullMode.PULL_NONE)
-pins.set_pull(DigitalPin.P16, PinPullMode.PULL_NONE)
+pins.set_pull(DigitalPin.P8, PinPullMode.PULL_UP)
+pins.set_pull(DigitalPin.P13, PinPullMode.PULL_UP)
+pins.set_pull(DigitalPin.P14, PinPullMode.PULL_UP)
+pins.set_pull(DigitalPin.P15, PinPullMode.PULL_UP)
+pins.set_pull(DigitalPin.P16, PinPullMode.PULL_UP)
+maxMotorSpeed = 255
 lMotorMap = [[-1, -1, -1, -1, -1, -0.75, -0.5, -0.25, 0],
     [-1, -0.9375, -0.875, -0.8125, -0.75, -0.5, -0.25, 0, 0.25],
     [-1, -0.875, -0.75, -0.625, -0.5, -0.25, 0, 0.25, 0.5],
@@ -29,19 +46,31 @@ rMotorMap = [[0, -0.25, -0.5, -0.75, -1, -1, -1, -1, -1],
     [1, 1, 1, 1, 1, 0.75, 0.5, 0.25, 0]]
 
 def on_forever():
+    global lMotorSpeed, rMotorSpeed
     if pins.digital_read_pin(DigitalPin.P15) == 0:
+        pins.digital_write_pin(DigitalPin.P12, 1)
         radio.send_string("Open")
     elif pins.digital_read_pin(DigitalPin.P13) == 0:
+        pins.digital_write_pin(DigitalPin.P12, 1)
         radio.send_string("Close")
     elif pins.digital_read_pin(DigitalPin.P16) == 0:
         radio.send_string("LEDL")
     elif pins.digital_read_pin(DigitalPin.P14) == 0:
         radio.send_string("LEDR")
+    elif pins.digital_read_pin(DigitalPin.P8) == 0:
+        if pins.digital_read_pin(DigitalPin.P15) == 0:
+            radio.send_string("Honk")
+        else:
+            radio.send_string("showOff")
     else:
-        radio.send_value("lMotor",
-            ControlLeftMotor(pins.analog_read_pin(AnalogPin.P1),
-                pins.analog_read_pin(AnalogPin.P2)))
-        radio.send_value("rMotor",
-            ControlRightMotor(pins.analog_read_pin(AnalogPin.P1),
-                pins.analog_read_pin(AnalogPin.P2)))
+        lMotorSpeed = ControlMotor(pins.analog_read_pin(AnalogPin.P1),
+            pins.analog_read_pin(AnalogPin.P2),
+            "L")
+        rMotorSpeed = ControlMotor(pins.analog_read_pin(AnalogPin.P1),
+            pins.analog_read_pin(AnalogPin.P2),
+            "R")
+        radio.send_value("lMotor", lMotorSpeed)
+        radio.send_value("rMotor", rMotorSpeed)
+        led.plot_bar_graph(lMotorSpeed + rMotorSpeed, 511)
+        pins.digital_write_pin(DigitalPin.P12, 0)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,32 +1,260 @@
-function ControlRightMotor(x: number, y: number): number {
-    return Math.trunc(rMotorMap[Math.idiv(y - 500, 114) + 4.5][Math.idiv(x - 500, 114) + 4.5] * 255)
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("neoPixelR")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("neoPixelB")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("neoPixelG")
+    radio.sendString("Hiii")
+})
+function ControlMotor (x: number, y: number, motor: string) {
+    if (motor == "R") {
+        return Math.trunc(rMotorMap[Math.idiv(y - 500, 114) + 4.5][Math.idiv(x - 500, 114) + 4.5] * maxMotorSpeed)
+    } else {
+        return Math.trunc(lMotorMap[Math.idiv(y - 500, 114) + 4.5][Math.idiv(x - 500, 114) + 4.5] * maxMotorSpeed)
+    }
 }
-
-function ControlLeftMotor(x2: number, y2: number): number {
-    return Math.trunc(lMotorMap[Math.idiv(y2 - 500, 114) + 4.5][Math.idiv(x2 - 500, 114) + 4.5] * 255)
-}
-
-let rMotorMap : number[][] = []
-let lMotorMap : number[][] = []
+let rMotorSpeed = 0
+let lMotorSpeed = 0
+let rMotorMap: number[][] = []
+let lMotorMap: number[][] = []
+let maxMotorSpeed = 0
 radio.setGroup(1)
-pins.setPull(DigitalPin.P13, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P14, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P15, PinPullMode.PullNone)
-pins.setPull(DigitalPin.P16, PinPullMode.PullNone)
-lMotorMap = [[-1, -1, -1, -1, -1, -0.75, -0.5, -0.25, 0], [-1, -0.9375, -0.875, -0.8125, -0.75, -0.5, -0.25, 0, 0.25], [-1, -0.875, -0.75, -0.625, -0.5, -0.25, 0, 0.25, 0.5], [-1, -0.8125, -0.625, -0.375, -0.25, 0, 0.25, 0.5, 0.75], [-1, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1], [-0.75, -0.5, -0.25, 0, 0.25, 0.375, 0.625, 0.8125, 1], [-0.5, -0.25, 0, 0.25, 0.5, 0.625, 0.75, 0.875, 1], [-0.25, 0, 0.25, 0.5, 0.75, 0.8125, 0.875, 0.9375, 1], [0, 0.25, 0.5, 0.75, 1, 1, 1, 1, 1]]
-rMotorMap = [[0, -0.25, -0.5, -0.75, -1, -1, -1, -1, -1], [0.25, 0, -0.25, -0.5, -0.75, -0.8125, -0.875, -0.9375, -1], [0.5, 0.25, 0, -0.25, -0.5, -0.625, -0.75, -0.875, -1], [0.75, 0.5, 0.25, 0, -0.25, -0.375, -0.625, -0.8125, -1], [1, 0.75, 0.5, 0.25, 0, -0.25, -0.5, -0.75, -1], [1, 0.8125, 0.625, 0.375, 0.25, 0, -0.25, -0.5, -0.75], [1, 0.875, 0.75, 0.625, 0.5, 0.25, 0, -0.25, -0.5], [1, 0.9375, 0.875, 0.8125, 0.75, 0.5, 0.25, 0, -0.25], [1, 1, 1, 1, 1, 0.75, 0.5, 0.25, 0]]
-basic.forever(function on_forever() {
+pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+maxMotorSpeed = 255
+lMotorMap = [
+[
+-1,
+-1,
+-1,
+-1,
+-1,
+-0.75,
+-0.5,
+-0.25,
+0
+],
+[
+-1,
+-0.9375,
+-0.875,
+-0.8125,
+-0.75,
+-0.5,
+-0.25,
+0,
+0.25
+],
+[
+-1,
+-0.875,
+-0.75,
+-0.625,
+-0.5,
+-0.25,
+0,
+0.25,
+0.5
+],
+[
+-1,
+-0.8125,
+-0.625,
+-0.375,
+-0.25,
+0,
+0.25,
+0.5,
+0.75
+],
+[
+-1,
+-0.75,
+-0.5,
+-0.25,
+0,
+0.25,
+0.5,
+0.75,
+1
+],
+[
+-0.75,
+-0.5,
+-0.25,
+0,
+0.25,
+0.375,
+0.625,
+0.8125,
+1
+],
+[
+-0.5,
+-0.25,
+0,
+0.25,
+0.5,
+0.625,
+0.75,
+0.875,
+1
+],
+[
+-0.25,
+0,
+0.25,
+0.5,
+0.75,
+0.8125,
+0.875,
+0.9375,
+1
+],
+[
+0,
+0.25,
+0.5,
+0.75,
+1,
+1,
+1,
+1,
+1
+]
+]
+rMotorMap = [
+[
+0,
+-0.25,
+-0.5,
+-0.75,
+-1,
+-1,
+-1,
+-1,
+-1
+],
+[
+0.25,
+0,
+-0.25,
+-0.5,
+-0.75,
+-0.8125,
+-0.875,
+-0.9375,
+-1
+],
+[
+0.5,
+0.25,
+0,
+-0.25,
+-0.5,
+-0.625,
+-0.75,
+-0.875,
+-1
+],
+[
+0.75,
+0.5,
+0.25,
+0,
+-0.25,
+-0.375,
+-0.625,
+-0.8125,
+-1
+],
+[
+1,
+0.75,
+0.5,
+0.25,
+0,
+-0.25,
+-0.5,
+-0.75,
+-1
+],
+[
+1,
+0.8125,
+0.625,
+0.375,
+0.25,
+0,
+-0.25,
+-0.5,
+-0.75
+],
+[
+1,
+0.875,
+0.75,
+0.625,
+0.5,
+0.25,
+0,
+-0.25,
+-0.5
+],
+[
+1,
+0.9375,
+0.875,
+0.8125,
+0.75,
+0.5,
+0.25,
+0,
+-0.25
+],
+[
+1,
+1,
+1,
+1,
+1,
+0.75,
+0.5,
+0.25,
+0
+]
+]
+basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P15) == 0) {
+        pins.digitalWritePin(DigitalPin.P12, 1)
         radio.sendString("Open")
     } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
+        pins.digitalWritePin(DigitalPin.P12, 1)
         radio.sendString("Close")
     } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
-        radio.sendString("LEDL")
+        if (maxMotorSpeed < 245) {
+            maxMotorSpeed += 10
+        }
     } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
-        radio.sendString("LEDR")
+        if (maxMotorSpeed > 8) {
+            maxMotorSpeed += -10
+        }
+    } else if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+        radio.sendString("showOff")
     } else {
-        radio.sendValue("lMotor", ControlLeftMotor(pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P2)))
-        radio.sendValue("rMotor", ControlRightMotor(pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P2)))
+        lMotorSpeed = ControlMotor(pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P2), "L")
+        rMotorSpeed = ControlMotor(pins.analogReadPin(AnalogPin.P1), pins.analogReadPin(AnalogPin.P2), "R")
+        radio.sendValue("lMotor", lMotorSpeed)
+        radio.sendValue("rMotor", rMotorSpeed)
+        led.plotBarGraph(
+        lMotorSpeed + rMotorSpeed,
+        511
+        )
+        pins.digitalWritePin(DigitalPin.P12, 0)
     }
-    
 })
